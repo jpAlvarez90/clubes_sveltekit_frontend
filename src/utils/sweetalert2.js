9fb658a4e444fb95971f938255ef0153a49503f5
beltrancruz <20183ti009@gmail.com>
2021-10-15 12:05:33 -0500

    v0.4.0 Implementación de servicios (Módulos GUT)

---
+++ src/utils/sweetalert2.js
@@ -0,0 +1,13 @@
+export default{
+    question(){
+        return{
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }
+    }
+}
